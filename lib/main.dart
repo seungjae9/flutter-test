ed74c01b4c54a44fb08f15b099a1798b47541375
seungjae9 <moseung18@gmail.com>
2022-02-02 01:06:43 +0900

    커스텀위젯 과제끝

--- lib/main.dart
+++ lib/main.dart
@@ -10,121 +10,83 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          toolbarHeight: 50,
-          title: TextButton(
-            onPressed: () {},
-            child: Row(
-              children: const [
-                Text(
-                  '중앙로 248',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 18),
+        home: Scaffold(
+            appBar: AppBar(),
+            bottomNavigationBar: MyBottomAppBar(),
+            body: ListView(
+              children: [
+                Container(
+                  child: Row(
+                    children: [
+                      Image.network(
+                        'https://png.clipart.me/istock/previews/9349/93493545-people-icon.jpg',
+                        height: 20,
+                      ),
+                      SizedBox(width: 5),
+                      Text('홍길동'),
+                    ],
+                  ),
                 ),
-                Icon(
-                  Icons.arrow_drop_down,
-                  color: Colors.black,
-                )
+                Container(
+                  child: Row(
+                    children: [
+                      Image.network(
+                        'https://png.clipart.me/istock/previews/9349/93493545-people-icon.jpg',
+                        height: 20,
+                      ),
+                      SizedBox(width: 5),
+                      Text('홍길동'),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    children: [
+                      Image.network(
+                        'https://png.clipart.me/istock/previews/9349/93493545-people-icon.jpg',
+                        height: 20,
+                      ),
+                      SizedBox(width: 5),
+                      Text('홍길동'),
+                    ],
+                  ),
+                ),
               ],
-            ),
+            )));
+  }
+}
+
+class MyBottomAppBar extends StatelessWidget {
+  const MyBottomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+        child: Container(
+      height: 50,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          IconButton(
+            icon: Icon(Icons.phone),
+            onPressed: () {
+              print('call');
+            },
           ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.search),
-              color: Colors.black,
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.menu),
-              color: Colors.black,
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.notifications_outlined),
-              color: Colors.black,
-            ),
-          ],
-        ),
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Image(
-                image: NetworkImage('https://www.daangn.com/logo.png'),
-                width: 150,
-                height: 150,
-              ),
-              Container(
-                width: 200,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('카메라 팝니다'),
-                    Text('금호동 3가'),
-                    Text('7,000원'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [Icon(Icons.favorite), Text('4')],
-                    )
-                  ],
-                ),
-              )
-            ],
+          IconButton(
+            icon: Icon(Icons.message),
+            onPressed: () {
+              print('message');
+            },
           ),
-        ),
-        // body: Container(
-        //   height: 200,
-        //   child: Container(
-        //     margin: const EdgeInsets.all(10),
-        //     child: Row(
-        //       children: [
-        //         Container(
-        //           margin: const EdgeInsets.fromLTRB(5, 0, 0, 30),
-        //           child: const Image(
-        //             image: NetworkImage('https://www.daangn.com/logo.png'),
-        //             width: 100,
-        //             height: 100,
-        //             fit: BoxFit.fill,
-        //           ),
-        //         ),
-        //         Container(
-        //           margin: const EdgeInsets.all(10),
-        //           child: Column(
-        //             crossAxisAlignment: CrossAxisAlignment.start,
-        //             children: const [
-        //               SizedBox(
-        //                 child: Text(
-        //                   '캐논 DSLR 100D(단렌즈, \n 충전기 16기가 SD 포함',
-        //                   style: TextStyle(fontSize: 18),
-        //                 ),
-        //               ),
-        //               SizedBox(
-        //                 child: Text(
-        //                   '성동구 행당동 끌올 10분 전',
-        //                   style: TextStyle(fontSize: 13, color: Colors.grey),
-        //                 ),
-        //               ),
-        //               SizedBox(
-        //                 child: Text(
-        //                   '210,000원',
-        //                   style: TextStyle(
-        //                       fontSize: 13, fontWeight: FontWeight.w600),
-        //                 ),
-        //               ),
-        //             ],
-        //           ),
-        //         )
-        //       ],
-        //     ),
-        //   ),
-        // ),
+          IconButton(
+            icon: Icon(Icons.book),
+            onPressed: () {
+              print('phone_book');
+            },
+          ),
+        ],
       ),
-    );
+    ));
   }
 }
