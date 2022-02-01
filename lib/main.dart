fe0801f8d0e3b588fe83f415d0b75788ad5a89b5
seungjae9 <moseung18@gmail.com>
2022-02-02 01:31:28 +0900

    좋아요 stateful 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var a = 1;
+  var name = ['김영숙', '홍길동', '박승재'];
+  var like = [0, 0, 0];
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +23,19 @@ class MyApp extends StatelessWidget {
             appBar: AppBar(),
             bottomNavigationBar: MyBottomAppBar(),
             body: ListView.builder(
-              itemCount: 20,
+              itemCount: 3,
               itemBuilder: (context, item) {
                 return ListTile(
-                  leading: Image.network(
-                      'https://icons.iconarchive.com/icons/custom-icon-design/flatastic-11/256/Twitter-1-icon.png'),
-                  title: Text('홍길동'),
+                  leading: Text(like[item].toString()),
+                  title: Text(name[item]),
+                  trailing: TextButton.icon(
+                      onPressed: () {
+                        setState(() {
+                          like[item]++;
+                        });
+                      },
+                      icon: Icon(Icons.plus_one),
+                      label: Text('좋아요')),
                 );
               },
             )));
