5fa6bde74d21e12a6268a9bd7187d850d22dd77a
seungjae9 <moseung18@gmail.com>
2022-02-02 00:46:06 +0900

    커스텀위젯

--- lib/main.dart
+++ lib/main.dart
@@ -4,127 +4,36 @@ void main() {
   runApp(const MyApp());
 }
 
+///
+/// 커스텀 위젯 다는 방법 2가지
+/// 1. 변수화
+/// 2. class 화
+/// 
+/// 리스트뷰
+/// - 스크롤바 자동 생김
+/// - 스크롤 위치 감시 가능
+/// - 메모리 절약 기능
+///
+
+
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          toolbarHeight: 50,
-          title: TextButton(
-            onPressed: () {},
-            child: Row(
-              children: const [
-                Text(
-                  '중앙로 248',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 18),
-                ),
-                Icon(
-                  Icons.arrow_drop_down,
-                  color: Colors.black,
-                )
+        home: Scaffold(
+            appBar: AppBar(),
+            body: ListView(
+              children: [
+                Text('dd'),
+                Text('dd'),
+                Text('dd'),
+                Text('dd'),
               ],
-            ),
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.search),
-              color: Colors.black,
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.menu),
-              color: Colors.black,
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.notifications_outlined),
-              color: Colors.black,
-            ),
-          ],
-        ),
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Image(
-                image: NetworkImage('https://www.daangn.com/logo.png'),
-                width: 150,
-                height: 150,
-              ),
-              Container(
-                width: 200,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('카메라 팝니다'),
-                    Text('금호동 3가'),
-                    Text('7,000원'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [Icon(Icons.favorite), Text('4')],
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-        // body: Container(
-        //   height: 200,
-        //   child: Container(
-        //     margin: const EdgeInsets.all(10),
-        //     child: Row(
-        //       children: [
-        //         Container(
-        //           margin: const EdgeInsets.fromLTRB(5, 0, 0, 30),
-        //           child: const Image(
-        //             image: NetworkImage('https://www.daangn.com/logo.png'),
-        //             width: 100,
-        //             height: 100,
-        //             fit: BoxFit.fill,
-        //           ),
-        //         ),
-        //         Container(
-        //           margin: const EdgeInsets.all(10),
-        //           child: Column(
-        //             crossAxisAlignment: CrossAxisAlignment.start,
-        //             children: const [
-        //               SizedBox(
-        //                 child: Text(
-        //                   '캐논 DSLR 100D(단렌즈, \n 충전기 16기가 SD 포함',
-        //                   style: TextStyle(fontSize: 18),
-        //                 ),
-        //               ),
-        //               SizedBox(
-        //                 child: Text(
-        //                   '성동구 행당동 끌올 10분 전',
-        //                   style: TextStyle(fontSize: 13, color: Colors.grey),
-        //                 ),
-        //               ),
-        //               SizedBox(
-        //                 child: Text(
-        //                   '210,000원',
-        //                   style: TextStyle(
-        //                       fontSize: 13, fontWeight: FontWeight.w600),
-        //                 ),
-        //               ),
-        //             ],
-        //           ),
-        //         )
-        //       ],
-        //     ),
-        //   ),
-        // ),
-      ),
+            )
+        )
     );
   }
 }
