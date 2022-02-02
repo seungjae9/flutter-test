73cc9efaafa55602f970a4d77e4442b0290a88a6
seungjae9 <moseung18@gmail.com>
2022-02-02 14:22:41 +0900

    dialog 과제 끝

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 /// 해당하는 부모 context가 없을때 context 에러가 생긴다 이때, 부모를 만들어 주거나 / builder 로 족보를 만들어준다.
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -21,29 +21,39 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            floatingActionButton: Builder(builder: (context) {
-              return FloatingActionButton(
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        return Dialog(child: Text('안녕'));
-                      });
-                },
-              );
-            }),
-            appBar: AppBar(),
-            body: ListView.builder(
-              itemCount: 3,
-              itemBuilder: (context, item) {
-                return ListTile(
-                  leading: Image.network(
-                      'https://icons.iconarchive.com/icons/papirus-team/papirus-apps/256/twitter-icon.png'),
-                  title: Text(name[item]),
-                );
-              },
-            )));
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showDialog(
+                context: context,
+                barrierDismissible: false,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text(
+                      'Contact',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    content: TextField(),
+                    actions: [
+                      TextButton(onPressed: () {
+                        Navigator.of(context).pop();
+                      }, child: Text('Cancel')),
+                      TextButton(onPressed: () {}, child: Text('OK')),
+                    ],
+                  );
+                });
+          },
+        ),
+        appBar: AppBar(),
+        body: ListView.builder(
+          itemCount: 3,
+          itemBuilder: (context, item) {
+            return ListTile(
+              leading: Image.network(
+                  'https://icons.iconarchive.com/icons/papirus-team/papirus-apps/256/twitter-icon.png'),
+              title: Text(name[item]),
+            );
+          },
+        ));
   }
 }
